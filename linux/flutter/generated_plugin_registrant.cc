a553e50d76714850a0acebbf0da4e35c060486c8
Ivan Andrey Bolaños Massella <ivan.bolanos@goluuk.com>
2024-01-09 11:55:16 -0600

    image_picker and ocr sdk packages added

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_ocr_sdk/flutter_ocr_sdk_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_ocr_sdk_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterOcrSdkPlugin");
+  flutter_ocr_sdk_plugin_register_with_registrar(flutter_ocr_sdk_registrar);
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_ocr_sdk
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
